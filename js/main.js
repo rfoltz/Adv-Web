79c42ddfc12213e5a5ad845d9974131fd041b914
rfoltz <caboose512@hotmail.com>
2013-01-23 18:07:28 -0500

    Added real content to the page and the flexslider
    Added some content to the page as well as added the flexslider stuff to
    do the carousel.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+/*
+Source File: main.js
+Name:Robert Foltz
+Last Modified By: Robert Foltz
+Date Last Modified: 2013-01-23
+File Description: This is where most of the javascript is placed for my website.
+*/
 
+
+$(window).load(function(){
+$('.flexslider').flexslider({
+	animation:"slide",
+	slideshow:false
+});
+});

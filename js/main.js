c9b3ffd11cc85ea53aaca2d67cd7a94ddab4d5b7
rfoltz <caboose512@hotmail.com>
2013-01-24 23:13:33 -0500

    Added the Wireframes
    Added wireframes and changed the template page to reflect the comments
    made in the index. Also at the top of files I've created/edited I now
    have the correct format needed for the assignment.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 Source File: main.js
 Name:Robert Foltz
 Last Modified By: Robert Foltz
-Date Last Modified: 2013-01-23
+Website Name: Robert Folt's Portfolio
 File Description: This is where most of the javascript is placed for my website.
 */
 
-
+//On Window Load Initialize the FlexSlider Plugin.
 $(window).load(function(){
-$('.flexslider').flexslider({
-	animation:"slide",
-	slideshow:false
-});
+	$('.flexslider').flexslider({
+		animation:"slide", //change the animation style to slide
+		slideshow:false //make sure it doesn't auto rotate the images I find it annoying.
+	});
 });
